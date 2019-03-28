3f4640272674a3fefd69582041485a6a4f217e57
harjeevansingh <harjeevansingh01@gmail.com>
2019-03-28 14:34:17 +0530

    done

--- twitter_analyze.py
+++ twitter_analyze.py
@@ -1,1 +1,23 @@
 import tweepy
+from st_class import MyStreamListener
+
+# Store OAuth authentication credentials in relevant variables
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+# Pass OAuth details to tweepy's OAuth handler
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=['clinton', 'trump', 'sanders', 'cruz'])
+
